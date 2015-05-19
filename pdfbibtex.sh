d10d60c6003a643b4a5302444de1a7bbe9dde456
coyotte508 <coyotte508@gmail.com>
2015-05-19 13:38:32 +0200

    Handle .bib files better, and own directory in /tmp

--- pdfbibtex.sh
+++ pdfbibtex.sh
@@ -4,8 +4,12 @@ filename="${1##*/}"
 filename="${filename%.*}"
 DIR=$(dirname "$1")
 
-pdflatex --output-directory /tmp/ $1
-bibtex /tmp/$filename
-pdflatex --output-directory /tmp/ $1
-pdflatex --output-directory /tmp/ $1
-cp /tmp/$filename.pdf $DIR
+mkdir /tmp/pdfbibtex/
+pdflatex --output-directory /tmp/pdfbibtex/ $1
+cp *.bib /tmp/pdfbibtex/
+cd /tmp/pdfbibtex/
+bibtex $filename
+cd -
+pdflatex --output-directory /tmp/pdfbibtex/ $1
+pdflatex --output-directory /tmp/pdfbibtex/ $1
+cp /tmp/pdfbibtex/$filename.pdf $DIR
